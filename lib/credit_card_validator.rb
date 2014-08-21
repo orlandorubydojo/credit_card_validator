43ba6f3cac76131a1ec1a64cf551fd16d7420131
Nestor Castillo <nestor.castillo@izea.com>
2014-08-21 09:17:55 -0400

    progress from Dojo

--- lib/credit_card_validator.rb
+++ lib/credit_card_validator.rb
@@ -1,14 +1,45 @@
+require 'byebug'
 class CreditCardValidator
+
+  attr_reader :ccnumber
+  attr_reader :cctype
+
+  def initialize(number, type)
+    @ccnumber = number
+    @cctype = type
+  end
 
   # HINT:
   # create a method for each step involved in validating
   # a credit card number
   def is_card_number_valid?
-    # 1)From right moving left double value of every second digit
+    # 1)From right moving left double value of every second
+    #   digit
     # 2)If the product of this doubling operation is greater than 9
     #   then sum the digits of the products
     # 3)Take the sum of all the digits
     # 4)If the number is a multiple of 10 then the number is valid.
     # This methods should return true/false
+
   end
+
+  def reverse_number
+    @ccnumber.reverse
+  end
+
+  #stopped here.
+  def sum_double
+    reversed = reverse_number
+
+    (0..reversed.length-1).step(2) do |index|
+      temp_val = reversed[index].to_i * 2
+      # byebug
+      if temp_val > 9
+        temp_val = (temp_val/10) + (temp_val % 10)
+        reversed[index] = (temp_val[0].to_i + temp_val[1].to_i).to_s
+      end
+    end
+  reversed
+  end
+
 end
--- spec/credit_card_validator_spec.rb
+++ spec/credit_card_validator_spec.rb
@@ -1,16 +1,25 @@
-require './lib/cc'
+require './lib/credit_card_validator'
 
 describe CreditCardValidator do
   context '#initialize' do
-    it 'does some testing here' do
+    it 'should create a validator' do
       # Let us start with the initialize method
+      validator = CreditCardValidator.new("4444444444444444", "Visa")
+
+      expect(validator.ccnumber).to eq "4444444444444444"
+      expect(validator.cctype).to eq "Visa"
     end
   end
 
   context "#is_card_number_valid?" do
-    it 'does some testing here' do
-      # Look at hint in corresponding ruby file
-      # Perhaps a test for each step.
+    let(:validator) {CreditCardValidator.new("489", "Visa")}
+
+    it 'should reverse the cc number' do
+      expect(validator.reverse_number).to eq("984")
+    end
+
+    it 'should right to left double' do
+      expect(validator.sum_double).to eq("9164")
     end
   end
 end
